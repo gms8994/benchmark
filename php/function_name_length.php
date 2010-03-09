efbc2d2df8c1e1d964b1b5368c18aec20320fec4
Glendon Solsberry <gms8994@dp.cx>
2010-03-09 15:48:02 -0500

    Add new shorter length function name to compare

--- php/function_name_length.php
+++ php/function_name_length.php
@@ -5,11 +5,15 @@ require_once "bench.php";
 
 cmp_these(
     array(
+        'a();',
         'abcdefghijklmnopqrstuvwxyz();',
         'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz();'
     ), 10000000
 );
 
+function a() {
+    return false;
+}
 function abcdefghijklmnopqrstuvwxyz() {
     return false;
 }
