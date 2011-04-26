8a5c2f0266321fafd22db61ddc44b70998bfec1a
Glendon Solsberry <gms8994@dp.cx>
2011-04-26 16:11:31 -0400

    Compare print vs echo

--- php/print_vs_echo.php
+++ php/print_vs_echo.php
@@ -1,23 +1,14 @@
 #!/usr/bin/php
 <?php
 
-ob_start();
-
 require_once "bench.php";
 
 cmp_these(
     array(
         'p();',
         'e();' 
-    ), 1000000
+    ), 100000
 );
-
-$results = ob_get_contents();
-ob_end_clean();
-$results = preg_replace('/test/', '', $results);
-
-print $results;
-print "\n";
 
 function p() {
     print "test";
