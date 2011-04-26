d586d40628dd342b4a6cfa701bb766843dcca395
Glendon Solsberry <gms8994@dp.cx>
2011-04-26 16:16:13 -0400

    Migrate to use a single function for executing code

--- php/bench.php
+++ php/bench.php
@@ -3,34 +3,31 @@
 function timeit($code, $times, $name = '') {
     global $globals;
 
-    if ($name === '') { $name = $code; }
-
-    $total_time = 0;
-
-    for($i=0;$i<$times;$i++) {
-        $st = microtime(true);
-        eval($code);
-        $total_time += microtime(true) - $st;
-    }
-
-    echo sprintf("%s: %s iterations took %.02f%s at %.03f%s\n", $name, number_format($times, 0, '.', ','), $total_time, "s", $times / $total_time, '/s');
+	run_code_block($code, $times, $name);
 }
 
 function cmp_these($codes, $times) {
     global $globals;
 
     foreach ($codes as $name => $code) {
-
-        if ($name === '' || is_int($name)) { $name = $code; }
-
-        $total_time = 0;
-
-        for($i=0;$i<$times;$i++) {
-            $st = microtime(true);
-            eval($code);
-            $total_time += microtime(true) - $st;
-        }
-
-        echo sprintf("%s: %s iterations took %.02f%s at %.03f%s\n", $name, number_format($times, 0, '.', ','), $total_time, "s", $times / $total_time, '/s');
+		run_code_block($code, $times, $name);
     }
+}
+
+function run_code_block($code, $times, $name = '') {
+	global $globals;
+
+	if ($name === '' || is_int($name)) { $name = $code; }
+
+	$total_time = 0;
+
+	ob_start();
+	for($i=0;$i<$times;$i++) {
+		$st = microtime(true);
+		eval($code);
+		$total_time += microtime(true) - $st;
+	}
+	ob_end_clean();
+
+	echo sprintf("%s: %s iterations took %.02f%s at %.03f%s\n", $name, number_format($times, 0, '.', ','), $total_time, "s", $times / $total_time, '/s');
 }
