9dc30c7ad77bbaba953077e89444c6bcbd397e42
Glendon Solsberry <gms8994@dp.cx>
2014-07-14 16:27:24 -0400

    Display a nice textile table

--- php/bench.php
+++ php/bench.php
@@ -1,5 +1,31 @@
 <?php
 
+function display_output() {
+	global $rates;
+
+	$keys = array_keys($rates);
+
+	print "||_. |_. Rate ";
+	foreach ($keys as $key) {
+		print "|_. $key ";
+	}
+	print "|\n";
+
+	foreach ($keys as $key) {
+		print "|_. $key | ";
+		print sprintf("%d/s", $rates[$key]) . " | ";
+
+		foreach ($keys as $key2) {
+			if ($key == $key2) print " -- ";
+			else print sprintf("%d%%", ($rates[$key] / $rates[$key2]) * 100);
+
+			print " |";
+		}
+
+		print "\n";
+	}
+}
+
 function timeit($code, $times, $name = '') {
     global $globals;
 
@@ -12,10 +38,12 @@ function cmp_these($codes, $times) {
     foreach ($codes as $name => $code) {
 		run_code_block($code, $times, $name);
     }
+
+	display_output();
 }
 
 function run_code_block($code, $times, $name = '') {
-	global $globals;
+	global $globals, $rates;
 
 	if ($name === '' || is_int($name)) { $name = $code; }
 
@@ -31,4 +59,6 @@ function run_code_block($code, $times, $name = '') {
 	}
 
 	echo sprintf("\r%s: %s iterations took %.06f%s at %.03f%s\n", $name, number_format($times, 0, '.', ','), $total_time, "s", $times / $total_time, '/s');
+
+	$rates[$name] = $times / $total_time;
 }
